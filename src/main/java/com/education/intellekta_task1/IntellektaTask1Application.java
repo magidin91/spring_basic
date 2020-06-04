91a2116c17b7bc95d693c57cdcff16d966e25a82
magidin91 <magidin@mail.ru>
2020-06-04 18:42:18 +0500

    Initial commit

---
+++ src/main/java/com/education/intellekta_task1/IntellektaTask1Application.java
@@ -0,0 +1,13 @@
+package com.education.intellekta_task1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellektaTask1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellektaTask1Application.class, args);
+	}
+
+}
